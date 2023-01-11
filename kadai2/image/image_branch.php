dff32f6db8e76d21341ecd10838d1e4feb7eead2
oyu25 <220348@jc-21.jp>
2023-01-11 12:49:48 +0900

    1/11

---
+++ kadai2/image/image_branch.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <mate charset="utf-8">
+        <title>ろくまる農業</title>
+
+</head>
+
+<body>
+    <?php
+
+    if (isset($_POST["disp"]) == true) {
+        if (isset($_POST["procode"]) == false) {
+
+
+            header("Location:pro_ng.php");
+            exit();
+        }
+        $pro_code = $_POST["procode"];
+        header("Location:pro_disp.php?procode=" . $pro_code);
+        exit();
+    }
+
+    if (isset($_POST["add"]) == true) {
+        header("Location:pro_add.php");
+    }
+    if (isset($_POST["edit"]) == true) {
+        // print"修正ボタンが押されました。";
+
+        if (isset($_POST["procode"]) == false) {
+            header("Location:pro_ng.php");
+            exit();
+        }
+        $pro_code = $_POST["procode"];
+        header("Location:pro_edit.php?procode=" . $pro_code);
+        exit();
+    }
+
+    if (isset($_POST["delete"]) == true) {
+        // print"削除ボタンが押されました。";
+
+        if (isset($_POST["procode"]) == false) {
+            header("Location:pro_ng.php");
+            exit();
+        }
+        $pro_code = $_POST["procode"];
+        header("Location:pro_delete.php?procode=" . $pro_code);
+        exit();
+    }
+    ?>
+
+</body>
+
+</html>
---
+++ kadai2/image/image_edit_check.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <mate charset="utf-8">
+    <title>ろくまる農業</title>
+
+</head>
+<?php
+
+$pro_code = $_POST['code'];
+$pro_name = $_POST['name'];
+$pro_price = $_POST['price'];
+$pro_gazou_name_old = $_POST['gazou_name_old'];
+$pro_gazou = $_FILES['gazou'];
+
+$pro_code = htmlspecialchars($pro_code, ENT_QUOTES, 'UTF-8');
+$pro_name = htmlspecialchars($pro_name, ENT_QUOTES, 'UTF-8');
+$pro_price = htmlspecialchars($pro_price, ENT_QUOTES, 'UTF-8');
+
+if ($pro_name == '') {
+  print '商品名が入力されていません。<br/>';
+} else {
+  print '商品名:';
+  print $pro_name;
+  print '<br\>';
+}
+
+if (preg_match('/\A[0-9]+\z/', $pro_price) == 0) {
+  print '価格をきちんと入力てください。<br\/>';
+} else {
+  print '価格';
+  print $pro_price;
+  print '円<br/>';
+}
+
+if ($pro_gazou['size'] > 0) {
+  if ($pro_gazou['size'] > 1000000) {
+    print '画像サイズが大き過ぎます';
+  } else {
+    move_uploaded_file($pro_gazou['tmp_name'], './gazou/' . $pro_gazou['name']);
+    print '<img src="./gazou/' . $pro_gazou['name'] . '">';
+    print '<br/>';
+  }
+}
+
+if ($pro_name == '' || preg_match('/\A[0-9]+\z/', $pro_price) == 0 || $pro_gazou['size'] > 1000000) {
+  print '<form>';
+  print '<button type="button" onclick="history.back()">戻る</button>';
+  print '</form>';
+} else {
+  print '上記のように変更します。<br/>';
+  print '<form method="post" action="pro_edit_done.php">';
+  print '<input type="hidden" name="code" value="' . $pro_code . '">';
+  print '<input type="hidden" name="name" value="' . $pro_name . '">';
+  print '<input type="hidden" name="price" value="' . $pro_price . '">';
+  print '<input type="hidden" name="gazou_name_old" value="' . $pro_gazou_name_old . '">';
+  print '<input type="hidden" name="gazou_name" value="' . $pro_gazou['name'] . '">';
+  print '<br/>';
+  print '<button type="button" onclick="history.back()">戻る</button>';
+  print '<button type="submit">OK</button>';
+  print '</form>';
+}
+?>
+
+<body>
+
+</body>
+
+</html>
