af76e4092da0060ca1b8a0c3e3698805b545e01f
oyu25 <220348@jc-21.jp>
2023-01-18 13:46:48 +0900

    1/18

--- kadai2/image_add.php
+++ kadai2/image_add.php
@@ -11,12 +11,24 @@
     <h1>画像の登録</h1>
     </br>
     <form method="post" action="image_add_check.php" enctype="multipart/form-data">
-        画像のタイトル
-        <input type="text" name="title" style="width:200px"></br>
-        画像の説明
-        <input type="text" name="description" style="width:200px;"></br>
-        画像ファイルの選択
-        <input type="file" name="file" style="width: 400px"></br>
+        <table solid="1">
+            <tr>
+                <th>画像のタイトル
+                    <input type="text" name="title" style="width:200px"></br>
+                </th>
+            </tr>
+
+            <tr>
+                <th>画像の説明
+                    <input type="text" name="description" style="width:200px;"></br>
+                </th>
+            </tr>
+            <tr>
+                <th>画像ファイルの選択
+                    <input type="file" name="file" style="width: 400px"></br>
+                </th>
+            </tr>
+        </table>
 
         <button type="button" onclick="history.back()">戻る</button>
         <button type="submit">ＯＫ</button>
--- kadai2/image_list.php
+++ kadai2/image_list.php
@@ -2,13 +2,16 @@
 <html lang="ja">
 
 <head>
-    <mate charset="utf-8">
-        <title>課題２</title>
 
+    <mate charset="utf-8">
+
+        <title>課題２</title>
+        <h1>画像一覧</h1>
 </head>
 
 <body>
-<link rel="stylesheet" href="image-list.css">
+
+    <link rel="stylesheet" href="image_list.css">
 
     <?php
     try {
@@ -24,26 +27,25 @@
 
         $dbh = null;
 
-              print "<form method='post' action='image_branch.php'>";
+        print "<form method='post' action='image_branch.php'>";
         while (true) {
             $rec = $stmt->fetch(PDO::FETCH_ASSOC);
 
             if ($rec == false) {
                 break;
             }
-            print '<input type="radio" name="imagecode" value="'  .$rec["id"]. '">';
-            print $rec["title"].'---';
-            print $rec['description'];
+            print '<input type="radio" name="imagecode" value="'  . $rec["id"] . '">';
+            print $rec["title"] ;
             print "<br/>";
         }
-        print'<button type="submit" name="disp">参照</button>';
-        print'<button type="submot" name="add">追加</button>';
+        print '<button type="submit" name="disp">参照</button>';
+        print '<button type="submot" name="add">追加</button>';
         print '<button type="submit" name="delete">削除</button>';
         print "</form>";
     } catch (Exception $e) {
 
         print "ただいま障害により大変ご迷惑をおかけしております。";
-        print"<br/>" .$e->getMessage();
+        print "<br/>" . $e->getMessage();
         exit();
     }
     ?>
