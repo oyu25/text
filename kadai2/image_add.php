4adbae46df66897ac206ea272cb0880334514958
oyu25 <220348@jc-21.jp>
2023-01-24 13:44:53 +0900

    1/24

--- kadai2/image_add.php
+++ kadai2/image_add.php
@@ -11,27 +11,24 @@
     <h1>画像の登録</h1>
     </br>
     <form method="post" action="image_add_check.php" enctype="multipart/form-data">
-        <table solid="1">
+        <table border="1" height="200" cellpadding="10" align="center">
             <tr>
-                <th>画像のタイトル
-                    <input type="text" name="title" style="width:200px"></br>
-                </th>
-            </tr>
-
+                <th>画像のタイトル</th>
+                <th><input type="text" name="title" style="width:400px"></br></th>
+</tr>
+                <tr>
+                    <th>画像の説明</th>
+                    <th><textarea cols="50" rows="5" name="description" style="width:400px;"></textarea></th>
+                </tr>
             <tr>
-                <th>画像の説明
-                    <input type="text" name="description" style="width:200px;"></br>
-                </th>
-            </tr>
-            <tr>
-                <th>画像ファイルの選択
-                    <input type="file" name="file" style="width: 400px"></br>
-                </th>
+                <th>画像ファイルの選択</th>
+                <th><input type="file" name="file" style="width: 400px"></br></th>
             </tr>
         </table>
-
-        <button type="button" onclick="history.back()">戻る</button>
-        <button type="submit">ＯＫ</button>
+        <div align="center">
+            <button type="button" onclick="history.back()">戻る</button>
+            <button type="submit">ＯＫ</button>
+        </div>
 
     </form>
 
